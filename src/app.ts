9b2f06ed666c89f777f1ee1fd6327379b6b8dfd0
Yevhenii2022 <3starosta3@gmail.com>
2023-08-15 09:42:33 +0300

    add project

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-const button = document.querySelector('button')! as HTMLButtonElement;
+const button = document.querySelector('button')!;
 const input1 = document.getElementById('num1')! as HTMLInputElement;
 const input2 = document.getElementById('num2')! as HTMLInputElement;
 
@@ -9,5 +9,3 @@ function add(num1: number, num2: number) {
 button.addEventListener('click', function () {
 	console.log(add(+input1.value, +input2.value));
 });
-
-// tsc -w app.ts
